f949481f8c20c0e0a8b9a677dd54c6f6d2595964
paulVer777 <pawel.gitara@onet.eu>
2018-04-25 11:47:32 +0200

    nowy komponent

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import MyFirstComponent from './components/MyFirstComponent'  //importujemy komponent z pliku MyFirstC.js
-
+import Add from './components/Sum'
 
 const App = () =>  //komponent glowny ktory jest funkcja i do tego komponentu wstawiamy nasze componenty
 
     (
-        <MyFirstComponent name="Mateusz"/> // atrubut trafia do props
+        <div>
+
+            <MyFirstComponent name="Mateusz"/>
+
+            {/*// atrubut trafia do props*/}
+
+            <Add numberA={5} numberB={10} />
+
+        </div>
+
+
     );
 
+export default App
 
-console.log(App());
 
 
-export default App;
+
+
---
+++ src/components/Sum.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Add=(props)=>(
+
+    <div>
+
+        Suma {props.numberA} i {props.numberB} jest równa {props.numberA + props.numberB}
+
+
+    </div>
+
+
+);
+
+
+export default Add

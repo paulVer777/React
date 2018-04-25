0bfe13b8c3c836802cba612f99495d99ed507583
paulVer777 <pawel.gitara@onet.eu>
2018-04-25 12:27:53 +0200

    style css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
 
+const styles={
+
+    header:{
+    color:'red',
+    margin:20,
+    display:'inline'
+}
+
+}
+
+
+
 const Hed=(props)=>(
 
     <div>
 
-       <h1>{props.text}</h1>
+       <h1
+
+           style={styles.header //obiekt jsx a w nim obiekt css
+
+
+           }
+
+
+       >{props.text}</h1>
 
 
     </div>

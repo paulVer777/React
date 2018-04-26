8c237ebdfc1a253a46ea897afea2d0be17e2b4db
paulVer777 <pawel.gitara@onet.eu>
2018-04-26 10:03:28 +0200

    onclick

--- src/components/CounterWithState.js
+++ src/components/CounterWithState.js
@@ -1,35 +1,42 @@
 import React from 'react'
 
 
-class CounterWithState extends React.Component{
+class CounterWithState extends React.Component {  //propsy przechodza do glownej klasy React.Components
+
+    state = {                        //stan jest z wewnatrz, komponent sie przerenderowywuje kiedy zzmieni sie stan
+
+        count: 0
 
 
-    render(){
-
-       return (
-
-           <div>
-
-               <h1>Tu bedzie liczba</h1>
-               <div>
-                   <button>-</button>
-                   <button>+</button>
+    };
 
 
-               </div>
+    render() {
+
+        const minusBtnClick = () => this.state.count--; //trzeba odświeżyć żeby pokazać wynik na ekranie
 
 
-           </div>
+        return (
+
+            <div>
+
+                <h1>{this.state.count}</h1>
+                <div>
+                    <button onClick={minusBtnClick}>-</button>
+                    <button>+</button>
 
 
-       )
+                </div>
 
+
+            </div>
+
+
+        )
 
 
     }
 }
 
 
-
-
-export  default CounterWithState
+export default CounterWithState
